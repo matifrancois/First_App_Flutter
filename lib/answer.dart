48ba9d3f7bbdf471395f1b11828a5785e75f83f2
Matias Francois <mfrancois@itba.edu.ar>
2020-08-25 02:29:06 -0300

    little app working

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function hander;
+  final String answerText;
 
-  Answer(this.hander);
+  Answer(this.hander, this.answerText);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: RaisedButton(
-        child: Text('Blue'),
+        child: Text(answerText),
         color: Colors.blue[300],
+        textColor: Colors.white,
         onPressed: hander,
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,20 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+
   var _questions = [
-    'What\'s your favorite color?',
-    'What\'s your favorite animal?'
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White']
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Cat', 'Dolphin', 'Parrot', 'Dog']
+    },
+    {
+      'questionText': 'What\'s your favorite day?',
+      'answers': ['Monday', 'Friday', 'Saturday', 'Sunday']
+    },
   ];
   //Metodo de la clase
   void _answerQuestion() {
@@ -39,10 +50,21 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(_questions[_questionIndex]),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
+            Question(
+              _questions[_questionIndex]['questionText'],
+            ),
+            //"as list of strings" es necesario para indicarle a visual que sabes
+            //de lo que estas hablando y que no te tire error (el no sabe de que hablas)
+            //.map te permite recorrer todos los elementos de la lista y para c/u
+            //realizar una funcion determinada.
+            //los 3 puntitos lo que hacen es que toman una lista y sacan los valores
+            //de esa lista y crean una nueva lista, esto sirve para por ejemplo
+            //evitar problemas con tener lista de listas.
+            ...(_questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
+            //.toList() indica que lo queres convertir a lista.
           ],
         ),
       ),
